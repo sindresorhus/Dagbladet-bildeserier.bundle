c989aa60ca9fbeb77e785ba4fba0bd4bf29c1857
sander1 <sander@plexapp.com>
2011-12-05 00:44:07 +0100

    Change xpath to get galleries with thumbs only; Remove lxml necessity

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,5 +1,3 @@
-from datetime import date
-import lxml
 import re
 
 
@@ -14,51 +12,58 @@ def Start():
 	Plugin.AddViewGroup('Pictures', viewMode='Coverflow', mediaType='photos')
 	Plugin.AddViewGroup('List', viewMode='List', mediaType='items')
 	MediaContainer.title1 = PLUGIN_TITLE
-	MediaContainer.viewGroup = 'InfoList'
+	MediaContainer.viewGroup = 'List'
 	MediaContainer.art = R(ART)
 	DirectoryItem.thumb = R(ICON)
+	HTTP.CacheTime = CACHE_1HOUR
+	HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) AppleWebKit/534.52.7 (KHTML, like Gecko) Version/5.1.2 Safari/534.52.7'
 
 
 def MainMenu():
 	dir = MediaContainer(viewGroup='Pictures')
 	url = 'http://www.dagbladet.no/bildeserier/'
 	# Get all links to articles
-	links = HTML.ElementFromURL(url).xpath('//div[@id="rowDZ"]//div[contains(@class,"saveableWo")]/a')
+	links = HTML.ElementFromURL(url).xpath('//div[@id="rowDZ"]//div[contains(@class,"saveableWo")]/a/img/parent::a')
+
 	for link in links:
 		title = link.get('title')
 		url = link.get('href')
-		# Only get galleries with thumbs
-		try:
-			thumb = link.xpath('img')[0].get('src')
-			dir.Append(Function(DirectoryItem(ImageViewer, title=title, thumb=thumb), title=title, url=url))
-		except:
-			pass
+		thumb = link.xpath('img')[0].get('src')
+		dir.Append(Function(DirectoryItem(ImageViewer, title=title, thumb=thumb), title=title, url=url))
+
 	return dir
 
 
-def ImageViewer(sender, title='', url=''):
-	dir = MediaContainer(viewGroup='List')
+def ImageViewer(sender, title, url):
+	dir = MediaContainer()
 	photos = HTML.ElementFromURL(url).xpath('//div[@id="galleryViewPhotos"]//div[@class="panel"]')
 	# Check if it's the new gallery, otherwise scrape the old
 	if len(photos):
 		for photo in photos:
-			summary = photo.xpath('div[@class="panel-overlay"]')[0].text_content().strip()
+			summary = photo.xpath('./div[@class="panel-overlay"]//text()')
+			summary = ' '.join(summary).strip()
 			summary = re.sub(r'Foto:', '\nFoto:', summary)
-			title = summary.split(':')[0]
-			src = photo.xpath('img[@class="slideImage"]')[0].get('src')
+			title = summary.split(':')[0].strip()
+			try:
+				summary = summary.split(':')[1].strip()
+			except:
+				pass
+			src = photo.xpath('./img[@class="slideImage"]')[0].get('src')
 			dir.Append(PhotoItem(src, title=title, summary=summary))
-		return dir
 	else:
 		# Since the images is in JS code, we need to parse out what we need.
 		# This can be removed when the old player is phased out.
-		html = HTML.ElementFromURL(url).xpath('//div[@id="dzImagesPano"]//script[@type="text/javascript"]')
-		html_string = lxml.html.tostring(html[0])
-		matches = re.findall(r'addToSlideshow\((.+?)\)', html_string)
+		slideshow = HTML.ElementFromURL(url).xpath('//div[@id="dzImagesPano"]//script[@type="text/javascript"]/text()')[0]
+		matches = re.findall(r'addToSlideshow\((.+?)\);', slideshow)
+
 		for match in matches:
-			item = match.split(',')
-			frag = lxml.html.fragment_fromstring(item[3], create_parent='div')
-			title = frag.xpath('b')[0].text_content()
-			src = item[2].replace('"', '')
-			summary = frag.xpath('text()')[1]
+			item = match.split(',"')
+			src = item[2].rstrip('"')
+
+			text = HTML.ElementFromString( item[3].rstrip('"') )
+			title = text.xpath('//b')[0].text.rstrip(': ').replace("\\'", "'")
+			summary = text.xpath('//text()')[1].replace("\\'", "'")
+
 			dir.Append(PhotoItem(src, title=title, summary=summary))
-		return dir
+
+	return dir
